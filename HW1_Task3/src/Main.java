baf56f051a5c6973401bd2fa3be37b320f41b6c2
Rituparn Shukla <shukla.r@phystech.edu>
2024-10-22 23:15:14 +0300

    Made the following changes:
    1. Put the KMP class logic in a different class
    
    2. Refactored the Prefix function into a seperate method
    
    3. Added try-catch
    
    }

--- HW1_Task3/src/Main.java
+++ HW1_Task3/src/Main.java
@@ -1,9 +1,10 @@
+package HW1_Task3.src;
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void kmpSearch(String text, String pattern) {
-        int textLen = text.length();
+    public static int [] prefixFunction(String pattern){
         int patternLen = pattern.length();
         int[] pi = new int[patternLen];
         int j = 0;
@@ -16,27 +17,39 @@ public class Main {
             }
             pi[l] = j;
         }
+        return pi;
 
-        int k = 0;
-        for (int i = 0; i < textLen; i++) {
-            while (k > 0 && pattern.charAt(k) != text.charAt(i)) {
-                k = pi[k - 1];
-            }
-            if (pattern.charAt(k) == text.charAt(i)) {
-                k++;
-            }
-            if (k == patternLen) {
-                System.out.print((i - patternLen + 1) + " ");
-                k = pi[k - 1];
+    }
+    public static class KMP{
+        public static void kmpSearch(String text, String pattern) {
+            int textLen = text.length();
+            int patternLen = pattern.length();
+            int [] prefixString= prefixFunction(pattern);
+            int k = 0;
+            for (int i = 0; i < textLen; i++) {
+                while (k > 0 && pattern.charAt(k) != text.charAt(i)) {
+                    k = prefixString[k - 1];
+                }
+                if (pattern.charAt(k) == text.charAt(i)) {
+                    k++;
+                }
+                if (k == patternLen) {
+                    System.out.print((i - patternLen + 1) + " ");
+                    k = prefixString[k - 1];
+                }
             }
         }
+
+
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String pattern = br.readLine();
-        String text = br.readLine();
-        br.close();
-        kmpSearch(text, pattern);
+    public static void main(String[] args)  {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                String pattern = br.readLine();
+                String text = br.readLine();
+                KMP.kmpSearch(text, pattern);
+            }catch (Exception e) {
+                System.out.println("following problem occurred " + e.getMessage());
+            }
     }
 }
