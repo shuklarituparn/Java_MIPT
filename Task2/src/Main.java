573987f77a3695fc4bca67a0db1bce83e5909909
Rituparn Shukla <shukla.r@phystech.edu>
2024-10-24 18:01:14 +0300

    Reformatted code

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -4,6 +4,12 @@ import java.util.*;
 
 public class Main {
 
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        BinaryTree.readInput(tree);
+        BinaryTree.printTree(tree);
+    }
+
     static class Node {
         int data;
         Node leftChild;
@@ -17,8 +23,45 @@ public class Main {
     }
 
     static class BinaryTree {
+        static HashSet<Integer> elementDict = new HashSet<>();
         Node root;
-        static HashSet<Integer> elementDict= new HashSet<>();
+
+        private static void findLeaves(Node current, List<Integer> leaves) {
+            if (current == null) {
+                return;
+            }
+            if (current.leftChild == null && current.rightChild == null) {
+                leaves.add(current.data);
+            } else {
+                findLeaves(current.leftChild, leaves);
+                findLeaves(current.rightChild, leaves);
+            }
+        }
+
+        public static void readInput(BinaryTree tree) {
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    int input = scanner.nextInt();
+                    if (input == 0) {
+                        break;
+                    }
+                    if (tree.addElement(input)) {
+                        BinaryTree.elementDict.add(input);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Following error happened " + e.getMessage());
+            }
+
+        }
+
+        public static void printTree(BinaryTree tree) {
+            List<Integer> leaves = tree.getLeaves();
+            for (int leaf : leaves) {
+                System.out.print(leaf + " ");
+            }
+            System.out.println();
+        }
 
         private Node addElement(Node current, int data) {
             if (current == null) {
@@ -33,24 +76,12 @@ public class Main {
         }
 
         public boolean addElement(int data) {
-            if (elementDict.contains(data)){
+            if (elementDict.contains(data)) {
                 return false;
             }
             root = addElement(root, data);
             return true;
 
-        }
-
-        private static void  findLeaves(Node current, List<Integer> leaves) {
-            if (current == null) {
-                return;
-            }
-            if (current.leftChild == null && current.rightChild == null) {
-                leaves.add(current.data);
-            } else {
-                findLeaves(current.leftChild, leaves);
-                findLeaves(current.rightChild, leaves);
-            }
         }
 
         public List<Integer> getLeaves() {
@@ -59,34 +90,5 @@ public class Main {
             Collections.sort(leaves);
             return leaves;
         }
-        public static void readInput(BinaryTree tree){
-            try (Scanner scanner= new Scanner(System.in)){
-                while (true) {
-                    int input = scanner.nextInt();
-                    if (input == 0) {
-                        break;
-                    }
-                    if (tree.addElement(input)){
-                        BinaryTree.elementDict.add(input);
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("Following error happened "+e.getMessage());
-            }
-
-        }
-        public static void printTree(BinaryTree tree){
-            List<Integer> leaves = tree.getLeaves();
-            for (int leaf : leaves) {
-                System.out.print(leaf + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        BinaryTree.readInput(tree);
-        BinaryTree.printTree(tree);
     }
 }
