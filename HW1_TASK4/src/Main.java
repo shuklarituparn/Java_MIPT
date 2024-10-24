68efdc77e0fcd77e90341fb243eed524b3c204f7
Rituparn Shukla <shukla.r@phystech.edu>
2024-10-24 18:02:59 +0300

    Reformatted code

--- HW1_TASK4/src/Main.java
+++ HW1_TASK4/src/Main.java
@@ -3,7 +3,10 @@ package HW1_TASK4.src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     private static int[] findTailsAndPositions(int[] nums, int[] tails, int[] positions) {
